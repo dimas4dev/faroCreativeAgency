9c12fe7c8678f3291c91aa58bc6e16b68a1d0805
Dimas Mendoza <dimasmendoza@ticsocial.com.co>
2022-05-31 17:03:18 -0500

    header responsive with hamburguer menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import fontawesome from "@fortawesome/fontawesome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheckSquare, faCoffee } from "@fortawesome/fontawesome-free-solid";
 
 import LogoFPC from "@assets/images/LOGO_FPC/FPCLogo.svg";
 import LogoFaro from "@assets/images/LOGO_FARO/faro4.svg";
@@ -20,15 +23,29 @@ const Header = () => {
           </li>
         </ul>
       </section>
-      <section className="header-menu">
-        <ul>
-          <li>Lo que somos </li>
-          <li> / </li>
-          <li>Nuestra Obra</li>
-          <li> / </li>
-          <li>Todo Cuenta</li>
-        </ul>
-      </section>
+      <nav className="nav">
+        <div className="nav__container">
+          <label htmlFor="menu" className="nav__label">
+            <FontAwesomeIcon
+              className="nav__img"
+              icon="fa-solid fa-bars"
+              size="lg"
+            />
+          </label>
+          <input className="nav__input" type="checkbox" id="menu" />
+          <div className="nav__menu">
+            <a href="#" className="nav__item">
+              Lo que somos{" "}
+            </a>
+            <a href="#" className="nav__item">
+              Nuestra Obra
+            </a>
+            <a href="#" className="nav__item">
+              Todo Cuenta
+            </a>
+          </div>
+        </div>
+      </nav>
     </header>
   );
 };
