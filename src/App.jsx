fc0c7c958dd63571275cd3b9ab354fb9450a0e87
Dimas Mendoza <dimasmendoza@ticsocial.com.co>
2022-06-09 17:02:51 -0500

    Push footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import { Header } from "@components/Header/Header";
 
 import "@source/App.scss";
 import { CarouselComponent } from "@components/Carousel/Carousel";
+import { Footer } from "@components/Footer/Footer";
 
 const App = () => {
   return (
     <>
       <Header />
       <CarouselComponent />
+      <Footer />
     </>
   );
 };
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,9 +3,28 @@ import React from "react";
 export const Footer = () => {
   return (
     <footer className="footer">
-      <span className="footer__text">
+      <div className="footer__title">
         <p>Unete a Nosotros</p>
-      </span>
+      </div>
+      <div className="footer__content">
+        <div className="footer__content--left">
+          <p>Contactanos</p>
+          <p>Linea Call Center: 3508204075</p>
+          <p>Linea Administrativa: +6014079353</p>
+          <p>fpc.comercial@faropsicopedagogia.org</p>
+        </div>
+        <div className="footer__content--right">
+          <span>
+            <p>@agenciafpc</p>
+          </span>
+          <span>
+            <p>
+              Cada vez que tomas un servicio con nosotros estas apoyando 13
+              proyectos sociales de:
+            </p>
+          </span>
+        </div>
+      </div>
     </footer>
   );
 };
