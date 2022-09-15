fc11d99d75230d4c534f3de1574eb74eab4a5512
Dimas Mendoza <damendoza98@gmail.com>
2022-09-15 18:52:20 -0500

    Update carousel

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -2,25 +2,36 @@ import React from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
+import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+// import "./styles.css";
+
 import BurbujasSombrero from "@assets/images/PiezasBanner/BannerFondoVerde.svg";
 import BannerPoderosa from "@assets/images/PiezasBanner/BannerPoderosa.svg";
 import BannerFuerza from "@assets/images/PiezasBanner/BannerFuerza.svg";
 import BannerCreativa from "@assets/images/PiezasBanner/BannerCreativa.svg";
 
+
+
 const CarouselComponent = () => {
   return (
-    <>
-      <Carousel>
-        <div>
-          <img alt="" src={BannerPoderosa} />
-          <p className="legend">Legend 1</p>
-        </div>
-        <div>
-          <img alt="" src={BurbujasSombrero} />
-          <p className="legend">Legend 1</p>
-        </div>
-      </Carousel>
-    </>
+    <Swiper
+
+      navigation={true}
+      pagination={true}
+      mousewheel={true}
+      keyboard={true}
+      modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+      className="carouselComponent"
+    >
+      <SwiperSlide className="carouselComponent__slide"> <img src={BannerCreativa} alt="" /></SwiperSlide>
+      <SwiperSlide className="carouselComponent__slide"><img src={BannerPoderosa} alt="" /></SwiperSlide>
+
+    </Swiper>
   );
 };
 
