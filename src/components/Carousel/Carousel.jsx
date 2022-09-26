74d954b0776f195047869be0929df4871613af0d
Dimas Mendoza <damendoza98@gmail.com>
2022-09-25 21:24:51 -0500

    delete react responsive carousel

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { Carousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
+
 
 import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
