d526f742e60bcf5a726ce4440d16b7d83463f504
Dimas Mendoza <damendoza98@gmail.com>
2022-11-14 21:10:00 -0500

    End of totally pages

---
+++ src/pages/Advisory/Advisory.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+
+
+const Advisory = () => {
+    return (
+        <>
+            <section className='Advisory'>
+                <div className='Advisory__title'>
+                    <h2 className='Advisory__title--modifier'>ASESORÍA</h2>
+
+                    <ul className="Advisory__title--modifierList">
+                        <li>Reunión con equipo de trabajo completo</li>
+                        <li>Sesión de asesoría en temas digitales</li>
+                    </ul>
+
+
+                </div>
+                <div className='Advisory__description'>
+                    <p>Nos encargamos de asesorarte sobre qué canal deberías usar de acuerdo a tu público objetivo y los gestionamos de manera profesional.</p>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export { Advisory }
---
+++ src/pages/SEO/SEO.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+
+
+const SEO = () => {
+    return (
+        <>
+            <section className='SEO'>
+                <div className='SEO__title'>
+                    <h2 className='SEO__title--modifier'>SEO</h2>
+
+                    <ul className="SEO__title--modifierList">
+                        <li>Redacción de artículo</li>
+                        <li>Optimización web</li>
+                        <li>Configuración de Google Search Console</li>
+                        <li>Configuración de Google My Business</li>
+                    </ul>
+
+
+                </div>
+                <div className='SEO__description'>
+                    <p>Posicionamos tu página dentro de los primeros resultados de búsqueda que te ayudará a lograr tu objetivo.</p>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export { SEO }
