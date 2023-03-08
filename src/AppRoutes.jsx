8c0260b3ef70546947c050b92ca72fbf15dcf1ee
Juan Marquez <marquez.cucho@gmail.com>
2023-03-07 19:49:20 -0500

    header

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -4,13 +4,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import HomeComponent from '@components/Home/index';
+import HeaderPage from "@components/Header/index";
 
 
 
 function AppRoutes() {
     return (
         <>
-            <HomeComponent />
+            <HeaderPage />
         </>
     );
 }
